718fe16e9af0804b48d0ab476571d699c0f318a9
mith_shaw <mithiles9shaw@gmail.com>
2026-02-21 14:51:29 +0530

    initial commit

---
+++ Git_Steps.py
@@ -0,0 +1,8 @@
+    git status
+    # git cofig --global user.email "mithilesh9shaw@gmail.com"
+    # git --config --global user.name "mithilesh9shaw" or "mith_shaw" : existing one
+    git init
+39:00   .git is hidden folder we need to enable/disable it
+        git status
+42:15   git branch : Branch details
+43:30   Initial commit is to register the master branch
